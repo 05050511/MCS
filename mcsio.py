2432a9edf4e564510add56fa45afc93df8a12ac2
05050511 <ccoo15151@gmail.com>
2018-11-29 16:01:33 +0800

    2018/11/29

--- mcsio.py
+++ mcsio.py
@@ -1,11 +1,39 @@
+#!/usr/bin/python3
+import sys
+import time
+import httplib as http
+import urllib
+import json
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+deviceId = "DV3KgWM9"
+deviceKey = "tKZNkaGjGYPLeY1o" 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = http.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException, socket.error) as ex: 
+			print( "Error: %s" %ex) 
+			time.sleep(10)
+			 # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
 
-while 'true'
-	SwitchStatus = GPIO.input(24)
-	if( SwitchStatus == 0):
-		ptint("Button presses")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+while True:
+	SwitchStatus = GPIO.input(4)
+	if(SwitchStatus == 0):
+		print('Button press')
+		time.sleep(1)
 	else:
-		print("Button released")
-payload = {"datapoints":[{"dataChnId":"Hum","values":{"value":humidity}},{"dataChnId":"Temp","values":{"value":temperature}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
+		print('Button released')
+		time.sleep(1)
+	payload = {"datapoints":[{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
+	post_to_mcs(payload)
